d5bcdd16854fb6afcaed51a79cfde6f1be66bb40
gounep <snusjmy@gmail.com>
2022-08-04 00:45:37 +0900

    add

--- shop/src/App.js
+++ shop/src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 import data from "./data";
 
 function App() {
-  let [shoe] = useState(data);
+  let [shoes] = useState(data);
 
   return (
     <div className="App">
@@ -31,24 +31,24 @@ function App() {
               src="https://codingapple1.github.io/shop/shoes1.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </div>
           <div className="col-md-4">
             <img
               src="https://codingapple1.github.io/shop/shoes2.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </div>
           <div className="col-md-4">
             <img
               src="https://codingapple1.github.io/shop/shoes3.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </div>
         </div>
       </div>
